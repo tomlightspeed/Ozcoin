a42cf3b94c635c4f7e44107d5bd96c614933ef21
tomlightspeed <tomlightspeed@gmail.com>
2011-06-18 03:11:33 -0700

    new stats layout

--- stats.php
+++ stats.php
@@ -18,18 +18,50 @@
 
 include ("includes/header.php");
 
-$stats = 'offline1';
-
-if($stats === 'offline') {
-echo "Stats Offline until we can update the mysql db hits, hopefully tommorow some time";
-	exit;
-}
-
 $bitcoinController = new BitcoinClient($rpcType, $rpcUsername, $rpcPassword, $rpcHost);
 
+?>
+<table><tr><td>
+<h1>Member Stats</h1><br/>
+<table border="1" cellspacing=0 cellpadding=5>
+<tr><td><b>Top 20 Hashrates</b></td></td><td><b>Top 20 Lifetime Shares</b></td></tr>
+<tr><td><table width=100% border="1">
+<tr><td>Rank</td><td>User Name</td><td>Hashrate</td></tr>
+<?php
 
+$result = mysql_query("SELECT id, hashrate FROM webUsers ORDER BY hashrate DESC LIMIT 30");
+$rank = 1;
+
+while ($resultrow = mysql_fetch_object($result)) {
+	$resdss = mysql_query("SELECT username FROM webUsers WHERE id=$resultrow->id");
+	$resdss = mysql_fetch_object($resdss);
+	$username = "$resdss->username";
+	echo "<tr><td>".$rank."</td><td>".$username."</td><td>".$resultrow->hashrate."</td></tr>";
+	$rank++;
+}
+
+?>
+</table></td>
+<td><table border="1" width=100%>
+<tr><td>User Name</td><td>Shares</td></tr>
+<?php
+
+$result = mysql_query("SELECT id, share_count, stale_share_count FROM webUsers ORDER BY share_count DESC LIMIT 30");
+
+while ($resultrow = mysql_fetch_object($result)) {
+	$resdss = mysql_query("SELECT username FROM webUsers WHERE id=$resultrow->id");
+	$resdss = mysql_fetch_object($resdss);
+	$username = "$resdss->username";
+	echo "<tr><td>".$username."</td><td>".($resultrow->share_count - $resultrow->stale_share_count)."</td></tr>";
+}
+
+?>
+</table></td></tr>
+</table>
+<?php
+
+echo "</td><td>"; // start server stats
 echo "<h2>Server Stats</h2><br/>";
-
 echo "Current Block: ".$bitcoinController->query("getblocknumber")."\n<br/>";
 echo "Current Difficulty: ".round($bitcoinController->query("getdifficulty"), 2)."<br/>";
 
@@ -38,7 +70,7 @@ if ($resultrow = mysql_fetch_object($result)) {
 	echo "<br>Last Block Found: ".$resultrow->blockNumber."<br/>";
 	echo "Confirmations: ".$resultrow->confirms."<br/>";
 	echo "Time: ".strftime("%B %d %Y %r",$resultrow->timestamp)."<br/>";
-	echo "<br><a href=blocks.php style=\"color: blue\">More Blocks</a><br>";
+	echo "<br><a href=blocks.php style=\"color: blue\">More Block Info</a><br>";
 }
 
 $res = mysql_query("SELECT count(webUsers.id) FROM webUsers WHERE hashrate > 0") or sqlerr(__FILE__, __LINE__);
@@ -48,40 +80,8 @@ $users = $row[0];
 echo "<br>Current Users Mining: ".$users."<br/>";
 echo "Current Total Miners: ".$settings->getsetting('currentworkers')."<br/>";
 
-?><br/>
+echo "</td></tr></table>";
 
-<h1>Member Stats</h1><br/>
-<table border="1" cellspacing=0 cellpadding=5>
-<tr><td><b>Top 20 Hashrates</b></td></td><td><b>Top 20 Lifetime Shares</b></td></tr>
-<tr><td><table width=100% border="1">
-	<tr><td>Rank</td><td>User Name</td><td>Hashrate</td></tr>
-	<?php
+include("includes/footer.php");
 
- 		$result = mysql_query("SELECT id, hashrate FROM webUsers ORDER BY hashrate DESC LIMIT 20");
- 		$rank = 1;
- 		while ($resultrow = mysql_fetch_object($result)) {
-			$resdss = mysql_query("SELECT username FROM webUsers WHERE id=$resultrow->id");
-			$resdss = mysql_fetch_object($resdss);
-			$username = "$resdss->username";
-			echo "<tr><td>".$rank."</td><td>".$username."</td><td>".$resultrow->hashrate."</td></tr>";
-			$rank++;
- 		}
-	?>
-</table></td>
-<td><table border="1" width=100%>
-	<tr><td>User Name</td><td>Shares</td></tr>
-	<?php
-		$result = mysql_query("SELECT id, share_count, stale_share_count FROM webUsers ORDER BY share_count DESC LIMIT 20");
-		while ($resultrow = mysql_fetch_object($result)) {
-
-$resdss = mysql_query("SELECT username FROM webUsers WHERE id=$resultrow->id");
-$resdss = mysql_fetch_object($resdss);
-$username = "$resdss->username";
-
-			echo "<tr><td>".$username."</td><td>".($resultrow->share_count - $resultrow->stale_share_count)."</td></tr>";
-		}
 ?>
-</table></td></tr>
-</table>
-
-<?php include("includes/footer.php"); ?>				
