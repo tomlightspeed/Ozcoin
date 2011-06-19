b7e5760abe93628a1fc4c546c1a7a558100e1038
tomlightspeed <tomlightspeed@gmail.com>
2011-06-19 11:42:04 -0700

    Edited stats.php via GitHub

--- stats.php
+++ stats.php
@@ -15,7 +15,7 @@
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // 	  BTC Donations: 163Pv9cUDJTNUbadV4HMRQSSj3ipwLURRc
-
+$pageTitle = "- Stats";
 include ("includes/header.php");
 
 $numberResults = 30;
