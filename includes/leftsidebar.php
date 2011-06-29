1aabd87180d7e1581a93f1f457ca04b8c57ee32d
tomlightspeed <tomlightspeed@gmail.com>
2011-06-28 18:37:01 -0700

    Edited includes/leftsidebar.php via GitHub

--- includes/leftsidebar.php
+++ includes/leftsidebar.php
@@ -1,30 +1,30 @@
 <?php
 /*
-Copyright (C) 41a240b48fb7c10c68ae4820ac54c0f32a214056bfcfe1c2e7ab4d3fb53187a0 Name Year (sha256)
+   Copyright (C) 41a240b48fb7c10c68ae4820ac54c0f32a214056bfcfe1c2e7ab4d3fb53187a0 Name Year (sha256)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
 
-Note From Author: Please donate at the following address: 1Fc2ScswXAHPUgj3qzmbRmwWJSLL2yv8Q
+   Note From Author: Please donate at the following address: 1Fc2ScswXAHPUgj3qzmbRmwWJSLL2yv8Q
 
-Modified for http://ozco.in/
-By Tom Lightspeed = tomlightspeed@gmail.com
-and Ryan Shaw (ryannathans) = ryannathans@hotmail.com = 1NasuTCjGjLDrTafSDR416FtLQidvjk99N
+   Modified for http://ozco.in/
+   By Tom Lightspeed = tomlightspeed@gmail.com
+   and Ryan Shaw (ryannathans) = ryannathans@hotmail.com = 1NasuTCjGjLDrTafSDR416FtLQidvjk99N
 */
 
 if(!$cookieValid){
@@ -38,8 +38,8 @@ Login:<br>
 <input type="password" name="password" onclick="this.value='';" onfocus="this.select()" onblur="this.value=!this.value?'password':this.value;" value="password" />
 <input type="submit" value="LOGIN">
 </form><br/>
-<form action="/login.php" method="post" login="lostForm" id="lostPassForm">
-<input type="submit" name="act" value="Lost Password">
+<form action="/lostpassword.php">
+<input type="submit" value="Lost Password">
 </form>
 </div>
 <?php
@@ -76,26 +76,26 @@ if(is_numeric($settings->getsetting('statstime'))) {
   else {
     echo 'Last Updated: ' . $lastupdatedtimei . ' minutes ago<br />';
   }
-  
+
   $nextupdatetime = 10 - $lastupdatedtimei;
-  
-  
+
+
   if($nextupdatetime == '1') {
     echo 'Next Update In: ' . $nextupdatetime . ' minute<br />';
   }
-  
+
   if($nextupdatetime < 0) {
-  
+
     $nextupdatetime = abs($nextupdatetime);
-	
-  	if($nextupdatetime < 1) {
-	  echo 'Server is ' . date("s", mktime(0, $nextupdatetime)) . ' seconds overdue refreshing';
-	}
+
+    if($nextupdatetime < 1) {
+      echo 'Server is ' . date("s", mktime(0, $nextupdatetime)) . ' seconds overdue refreshing';
+    }
     if($nextupdatetime == 1) {
-	  echo 'Server is ' . $nextupdatetime . ' minute overdue refreshing';
-	}
-	if($nextupdatetime > 1) {
-	  echo 'Server is ' . $nextupdatetime . ' minutes overdue refreshing';
+      echo 'Server is ' . $nextupdatetime . ' minute overdue refreshing';
+    }
+    if($nextupdatetime > 1) {
+      echo 'Server is ' . $nextupdatetime . ' minutes overdue refreshing';
     }
   }
   else {
